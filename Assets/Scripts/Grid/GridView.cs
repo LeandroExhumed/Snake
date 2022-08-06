b6247aae9ca8ae1ae85f3e0d810b58e7621edcee
Leandro Rodrigues Silva <leandroexhumed@gmail.com>
2022-08-06 14:13:13 -0300

    feat: changes on snake/level grid colors.

--- Assets/Scripts/Grid/GridView.cs
+++ Assets/Scripts/Grid/GridView.cs
@@ -7,16 +7,25 @@ namespace LeandroExhumed.SnakeGame.Grid
         [SerializeField]
         private SpriteRenderer sprite;
 
+        [SerializeField]
+        private Color primaryColor = Color.black;
+        [SerializeField]
+        private Color secondaryColor = Color.gray;
+
         public void Initialize (INode[,] array)
         {
             for (int x = 0; x < array.GetLength(0); x++)
             {
                 for (int y = 0; y < array.GetLength(1); y++)
                 {
-                    SpriteRenderer entry = Instantiate(sprite, new Vector3(x, y, 1), Quaternion.identity);
+                    SpriteRenderer entry = Instantiate(sprite, new Vector3(x, y, 0), Quaternion.identity, transform);
                     if ((x + y) % 2 == 0)
                     {
-                        entry.color = Color.green;
+                        entry.color = primaryColor;
+                    }
+                    else
+                    {
+                        entry.color = secondaryColor;
                     }
                 }
             }
